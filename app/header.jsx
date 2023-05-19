2745a00b5dc140eea619d06c2abb4a0fa61ac3a0
mnuralim <shadownur345.com>
2023-05-19 15:52:11 +0800

    update

--- app/header.jsx
+++ app/header.jsx
@@ -1,12 +1,33 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 import Navbar from "./navbar";
 
 const Header = () => {
+  const [navbarVisible, setNavbarVisible] = useState(true);
+
+  useEffect(() => {
+    let timeout;
+
+    const handleScroll = () => {
+      setNavbarVisible(false);
+
+      clearTimeout(timeout);
+
+      timeout = setTimeout(() => {
+        setNavbarVisible(true);
+      }, 200);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      clearTimeout(timeout);
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <>
-      <header>
-        <Navbar />
-      </header>
+      <header>{navbarVisible ? <Navbar /> : ""}</header>
     </>
   );
 };
--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ import Contact from "./contact";
 
 export default function Home() {
   return (
-    <main className="overflow-x-hidden bg-slate-50 lg:px-36">
+    <main className="overflow-x-hidden bg-slate-50 lg:px-36 ">
       <section className="relative">
         <div className="fixed bg-blue-400 rounded-full opacity-50 top-10 left-4 h-72 w-72 blur-3xl filter md:h-96 md:w-96"></div>
         <div className="fixed right-0 bg-yellow-200 rounded-full opacity-50 top-10 h-72 w-72 blur-3xl filter md:h-96 md:w-96"></div>
