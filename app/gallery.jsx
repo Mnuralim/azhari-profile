d4227dcc811753e926a4d582ff251ef6272ebb6e
mnuralim <shadownur345.com>
2023-05-21 13:58:37 +0800

    update

--- app/gallery.jsx
+++ app/gallery.jsx
@@ -11,7 +11,7 @@ const Gallery = () => {
           <h1 className="mb-10 text-4xl font-bold text-center mt-10 text-slate-700 lg:text-6xl lg:mt-24 dark:text-slate-50">
             Galeri <span className="text-indigo-500">Azhari</span>
           </h1>
-          <div className="flex bg-slate-300/30 rounded-xl mb-5">
+          <div className="flex bg-slate-300/30 rounded-xl mb-5 p-1 dark:bg-slate-700/30">
             <button
               className={`transition-colors duration-300 ease-linear w-full rounded-xl ${activeTab == "photo" ? "bg-indigo-500 text-white" : "bg-transparent"}  text-sm leading-5 font-medium py-3`}
               onClick={() => setActiveTab("photo")}
