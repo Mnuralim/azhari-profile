60dd43bdd6f9e8158c337be9c3702481f5d43238
mnuralim <shadownur345.com>
2023-05-21 13:53:35 +0800

    update

--- app/gallery.jsx
+++ app/gallery.jsx
@@ -1,9 +1,9 @@
 "use client";
-import React from "react";
-import { motion } from "framer-motion";
-import { galleryContainerVariant, galleryVariant } from "@/utils/motion";
+import React, { useState } from "react";
+import { GalleryCardPhoto, GalleryCardVideo } from "./galleryCard";
 
 const Gallery = () => {
+  const [activeTab, setActiveTab] = useState("photo");
   return (
     <>
       <section id="gallery" className="relative min-h-screen flex items-center p-5 mt-10 lg:mt-20">
@@ -11,44 +11,22 @@ const Gallery = () => {
           <h1 className="mb-10 text-4xl font-bold text-center mt-10 text-slate-700 lg:text-6xl lg:mt-24 dark:text-slate-50">
             Galeri <span className="text-indigo-500">Azhari</span>
           </h1>
-          <motion.div variants={galleryContainerVariant} initial="hidden" whileInView="show" className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-            <motion.div variants={galleryVariant}>
-              <h2 className="my-3 text-lg font-semibold text-slate-700 dark:text-slate-50">Silaturahmi Dengan Mahasiswa</h2>
-              <div className="w-full p-2 transition-colors duration-200 ease-in-out border border-slate-400 hover:bg-slate-400 dark:hover:bg-slate-400 dark:border-slate-400">
-                <div style={{ backgroundImage: "url('/img/mhs.jpg')", backgroundSize: "cover", backgroundRepeat: "no-repeat" }} className="w-full min-h-[250px] "></div>
-              </div>
-            </motion.div>
-            <motion.div variants={galleryVariant}>
-              <h2 className="my-3 text-lg font-semibold text-slate-700 dark:text-slate-50">Garda Azhari di Lakudo </h2>
-              <div className="w-full p-2 transition-colors duration-200 ease-in-out border border-slate-400 hover:bg-slate-400 dark:hover:bg-slate-400 dark:border-slate-400">
-                <div style={{ backgroundImage: "url('/img/galeri2.jpg')", backgroundSize: "cover", backgroundRepeat: "no-repeat" }} className="w-full min-h-[250px] "></div>
-              </div>
-            </motion.div>
-            <motion.div variants={galleryVariant}>
-              <h2 className="my-3 text-lg font-semibold text-slate-700 dark:text-slate-50">Kamomose Masyarakat Lakudo</h2>
-              <div className="w-full p-2 transition-colors duration-200 ease-in-out border border-slate-400 hover:bg-slate-400 dark:hover:bg-slate-400 dark:border-slate-400">
-                <div style={{ backgroundImage: "url('/img/galeri3.jpg')", backgroundSize: "cover", backgroundRepeat: "no-repeat" }} className="w-full min-h-[250px] "></div>
-              </div>
-            </motion.div>
-            <motion.div variants={galleryVariant}>
-              <h2 className="my-3 text-lg font-semibold text-slate-700 dark:text-slate-50">Silaturahmi Dengan Mahasiswa</h2>
-              <div className="w-full p-2 transition-colors duration-200 ease-in-out border border-slate-400 hover:bg-slate-400 dark:hover:bg-slate-400 dark:border-slate-400">
-                <div style={{ backgroundImage: "url('/img/mhs.jpg')", backgroundSize: "cover", backgroundRepeat: "no-repeat" }} className="w-full min-h-[250px] "></div>
-              </div>
-            </motion.div>
-            <motion.div variants={galleryVariant}>
-              <h2 className="my-3 text-lg font-semibold text-slate-700 dark:text-slate-50">Silaturahmi Dengan Mahasiswa</h2>
-              <div className="w-full p-2 transition-colors duration-200 ease-in-out border border-slate-400 hover:bg-slate-400 dark:hover:bg-slate-400 dark:border-slate-400">
-                <div style={{ backgroundImage: "url('/img/mhs.jpg')", backgroundSize: "cover", backgroundRepeat: "no-repeat" }} className="w-full min-h-[250px] "></div>
-              </div>
-            </motion.div>
-            <motion.div variants={galleryVariant}>
-              <h2 className="my-3 text-lg font-semibold text-slate-700 dark:text-slate-50">Silaturahmi Dengan Mahasiswa</h2>
-              <div className="w-full p-2 transition-colors duration-200 ease-in-out border border-slate-400 hover:bg-slate-400 dark:hover:bg-slate-400 dark:border-slate-400">
-                <div style={{ backgroundImage: "url('/img/mhs.jpg')", backgroundSize: "cover", backgroundRepeat: "no-repeat" }} className="w-full min-h-[250px] "></div>
-              </div>
-            </motion.div>
-          </motion.div>
+          <div className="flex bg-slate-300/30 rounded-xl mb-5">
+            <button
+              className={`transition-colors duration-300 ease-linear w-full rounded-xl ${activeTab == "photo" ? "bg-indigo-500 text-white" : "bg-transparent"}  text-sm leading-5 font-medium py-3`}
+              onClick={() => setActiveTab("photo")}
+            >
+              Foto
+            </button>
+
+            <button
+              className={`transition-colors duration-300 ease-linear w-full rounded-xl ${activeTab == "video" ? "bg-indigo-500 text-white" : "bg-transparent"}  text-sm leading-5 font-medium py-3`}
+              onClick={() => setActiveTab("video")}
+            >
+              Video
+            </button>
+          </div>
+          {activeTab == "photo" ? <GalleryCardPhoto /> : activeTab == "video" ? <GalleryCardVideo /> : ""}
         </div>
       </section>
     </>
---
+++ app/galleryCard.jsx
@@ -0,0 +1,78 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import { galleryContainerVariant, galleryVariant } from "@/utils/motion";
+
+export const GalleryCardPhoto = () => {
+  return (
+    <div>
+      <motion.div variants={galleryContainerVariant} initial="hidden" whileInView="show" className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        <motion.div variants={galleryVariant}>
+          <h2 className="my-3 text-lg font-semibold text-slate-700 dark:text-slate-50">Silaturahmi Dengan Mahasiswa</h2>
+          <div className="w-full p-2 transition-colors duration-200 ease-in-out border border-slate-400 hover:bg-slate-400 dark:hover:bg-slate-400 dark:border-slate-400">
+            <div style={{ backgroundImage: "url('/img/mhs.jpg')", backgroundSize: "cover", backgroundRepeat: "no-repeat" }} className="w-full min-h-[250px] "></div>
+          </div>
+        </motion.div>
+        <motion.div variants={galleryVariant}>
+          <h2 className="my-3 text-lg font-semibold text-slate-700 dark:text-slate-50">Garda Azhari di Lakudo </h2>
+          <div className="w-full p-2 transition-colors duration-200 ease-in-out border border-slate-400 hover:bg-slate-400 dark:hover:bg-slate-400 dark:border-slate-400">
+            <div style={{ backgroundImage: "url('/img/galeri2.jpg')", backgroundSize: "cover", backgroundRepeat: "no-repeat" }} className="w-full min-h-[250px] "></div>
+          </div>
+        </motion.div>
+        <motion.div variants={galleryVariant}>
+          <h2 className="my-3 text-lg font-semibold text-slate-700 dark:text-slate-50">Kamomose Masyarakat Lakudo</h2>
+          <div className="w-full p-2 transition-colors duration-200 ease-in-out border border-slate-400 hover:bg-slate-400 dark:hover:bg-slate-400 dark:border-slate-400">
+            <div style={{ backgroundImage: "url('/img/galeri3.jpg')", backgroundSize: "cover", backgroundRepeat: "no-repeat" }} className="w-full min-h-[250px] "></div>
+          </div>
+        </motion.div>
+        <motion.div variants={galleryVariant}>
+          <h2 className="my-3 text-lg font-semibold text-slate-700 dark:text-slate-50">Silaturahmi Dengan Mahasiswa</h2>
+          <div className="w-full p-2 transition-colors duration-200 ease-in-out border border-slate-400 hover:bg-slate-400 dark:hover:bg-slate-400 dark:border-slate-400">
+            <div style={{ backgroundImage: "url('/img/mhs.jpg')", backgroundSize: "cover", backgroundRepeat: "no-repeat" }} className="w-full min-h-[250px] "></div>
+          </div>
+        </motion.div>
+        <motion.div variants={galleryVariant}>
+          <h2 className="my-3 text-lg font-semibold text-slate-700 dark:text-slate-50">Silaturahmi Dengan Mahasiswa</h2>
+          <div className="w-full p-2 transition-colors duration-200 ease-in-out border border-slate-400 hover:bg-slate-400 dark:hover:bg-slate-400 dark:border-slate-400">
+            <div style={{ backgroundImage: "url('/img/mhs.jpg')", backgroundSize: "cover", backgroundRepeat: "no-repeat" }} className="w-full min-h-[250px] "></div>
+          </div>
+        </motion.div>
+        <motion.div variants={galleryVariant}>
+          <h2 className="my-3 text-lg font-semibold text-slate-700 dark:text-slate-50">Silaturahmi Dengan Mahasiswa</h2>
+          <div className="w-full p-2 transition-colors duration-200 ease-in-out border border-slate-400 hover:bg-slate-400 dark:hover:bg-slate-400 dark:border-slate-400">
+            <div style={{ backgroundImage: "url('/img/mhs.jpg')", backgroundSize: "cover", backgroundRepeat: "no-repeat" }} className="w-full min-h-[250px] "></div>
+          </div>
+        </motion.div>
+      </motion.div>
+    </div>
+  );
+};
+
+export const GalleryCardVideo = () => {
+  return (
+    <div>
+      <motion.div variants={galleryContainerVariant} initial="hidden" whileInView="show" className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2">
+        <motion.div variants={galleryVariant}>
+          <h2 className="my-3 text-lg font-semibold text-slate-700 dark:text-slate-50">Lorem ipsum dolor sit.</h2>
+          <div className="w-full p-2 transition-colors duration-200 ease-in-out border border-slate-400 hover:bg-slate-400 dark:hover:bg-slate-400 dark:border-slate-400">
+            {/* <div style={{ backgroundImage: "url('/img/mhs.jpg')", backgroundSize: "cover", backgroundRepeat: "no-repeat" }} className="w-full min-h-[250px] "></div> */}
+            <iframe width="100%" height="315" src="https://www.youtube.com/embed/7I9kKCnd_Ug" title="YouTube video player" frameborder="0" allowfullscreen></iframe>
+          </div>
+        </motion.div>
+        <motion.div variants={galleryVariant}>
+          <h2 className="my-3 text-lg font-semibold text-slate-700 dark:text-slate-50">Lorem ipsum dolor sit.</h2>
+          <div className="w-full p-2 transition-colors duration-200 ease-in-out border border-slate-400 hover:bg-slate-400 dark:hover:bg-slate-400 dark:border-slate-400">
+            {/* <div style={{ backgroundImage: "url('/img/mhs.jpg')", backgroundSize: "cover", backgroundRepeat: "no-repeat" }} className="w-full min-h-[250px] "></div> */}
+            <iframe width="100%" height="315" src="https://www.youtube.com/embed/7I9kKCnd_Ug" title="YouTube video player" frameborder="0" allowfullscreen></iframe>
+          </div>
+        </motion.div>
+        <motion.div variants={galleryVariant}>
+          <h2 className="my-3 text-lg font-semibold text-slate-700 dark:text-slate-50">Lorem ipsum dolor sit.</h2>
+          <div className="w-full p-2 transition-colors duration-200 ease-in-out border border-slate-400 hover:bg-slate-400 dark:hover:bg-slate-400 dark:border-slate-400">
+            <iframe width="100%" height="315" src="https://www.youtube.com/embed/7I9kKCnd_Ug" title="YouTube video player" frameborder="0" allowfullscreen></iframe>
+          </div>
+        </motion.div>
+      </motion.div>
+    </div>
+  );
+};
