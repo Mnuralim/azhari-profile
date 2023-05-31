dee3763632b215fa35074701d542c36989fee251
mnuralim <shadownur345.com>
2023-05-31 16:47:56 +0800

    update

--- app/home.jsx
+++ app/home.jsx
@@ -6,7 +6,7 @@ import { slideIn } from "@/utils/motion";
 export const HomePage = () => {
   return (
     <section id="home" className="relative p-5 lg:px-16">
-      <div className="relative z-50 flex flex-col items-center justify-center h-screen mt-5 md:gap-5 lg:flex-row-reverse lg:gap-48">
+      <div className="relative z-50 flex flex-col items-center justify-center h-screen  md:gap-5 lg:flex-row-reverse lg:gap-48">
         <motion.div variants={slideIn("right", "tween", 0.5, 1.5)} initial="hidden" whileInView="show" className="bg-indigo-500 w-80 h-80 blob opacity-100 overflow-hidden lg:h-[450px] lg:w-[450px]">
           <img src={"/img/azhari.png"} alt="azhari" className="mt-5 -mb-10" />
         </motion.div>
