b1b886ec6daf03b9ef2d9c4af5e0e93850609b4d
mnuralim <shadownur345.com>
2023-06-11 15:55:30 +0800

    update

--- app/home.jsx
+++ app/home.jsx
@@ -1,21 +1,38 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { slideIn } from "@/utils/motion";
 
 export const HomePage = () => {
+  const [screenSize, setScreenSize] = useState(0);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setScreenSize(window.innerWidth);
+
+      const handleResize = () => {
+        setScreenSize(window.innerWidth);
+      };
+
+      window.addEventListener("resize", handleResize);
+
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }
+  }, []);
   return (
     <section id="home" className="relative p-5 lg:px-16">
       <div className="relative z-50 flex flex-col items-center justify-center h-screen md:gap-5 lg:flex-row-reverse lg:gap-48">
         <motion.div variants={slideIn("right", "tween", 0.5, 1.5)} initial="hidden" whileInView="show" className="bg-indigo-500 w-80 h-80 blob opacity-100 overflow-hidden lg:h-[450px] lg:w-[450px]">
-          <img src={"/img/home5.png"} alt="azhari" className="-mb-10 md:ml-5 transform scale-x-[-1]" />
+          {screenSize >= 1024 ? <img src={"/img/home5.png"} alt="azhari" className="-mb-10 md:ml-3 transform scale-x-[-1]" /> : <img src={"/img/home2.png"} alt="azhari" className="-mb-10 md:ml-5 transform scale-x-[-1]" />}
         </motion.div>
         <motion.div variants={slideIn("left", "tween", 0.5, 1.5)} initial="hidden" whileInView="show" className="mt-10 text-center md:mt-0 md:text-left">
           <h1 className="text-2xl font-bold text-slate-700 md:text-4xl dark:text-slate-50 ">
             DR. Azhari,<span className="text-indigo-500">S.STP., M.Si</span>
           </h1>
           <h2 className="mt-4 text-xl font-medium text-slate-600 dark:text-slate-400">Rektor USN Periode 2014-2018, 2018-2022</h2>
-          <p className="mt-3 mb-5 text-lg text-slate-500 lg:w-96 dark:text-slate-50">"Mari kita desain pondasi dan pembangunan identitas masa kini dan masa depan jangka panjang Buton Tengah!"</p>
+          <p className="mt-3 mb-5 text-lg leading-10 text-slate-500 lg:w-96 dark:text-slate-50">" Mari kita desain pondasi dan pembangunan identitas masa kini dan masa depan jangka panjang Buton Tengah! "</p>
           {/* <div>
             <Link href={"#"} className="px-4 py-3 text-white bg-indigo-500 rounded ">
               <SendOutlinedIcon className="mr-2" />
