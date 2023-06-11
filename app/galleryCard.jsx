dd3b64a37e3cc2f1335523c0d44fdff684554484
mnuralim <shadownur345.com>
2023-06-11 15:15:43 +0800

    update

--- app/galleryCard.jsx
+++ app/galleryCard.jsx
@@ -2,29 +2,20 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { galleryContainerVariant, galleryVariant } from "@/utils/motion";
+import { galleryPhoto, galleryVideo } from "@/data/gallery";
 
 export const GalleryCardPhoto = () => {
   return (
     <div>
       <motion.div variants={galleryContainerVariant} initial="hidden" whileInView="show" className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-        <motion.div variants={galleryVariant}>
-          <h2 className="my-3 text-lg font-semibold text-slate-700 dark:text-slate-50">Silaturahmi Dengan Mahasiswa</h2>
-          <div className="w-full p-2 transition-colors duration-200 ease-in-out border border-slate-400 hover:bg-slate-400 dark:hover:bg-slate-400 dark:border-slate-400">
-            <div style={{ backgroundImage: "url('/img/mhs.jpg')", backgroundSize: "cover", backgroundRepeat: "no-repeat" }} className="w-full min-h-[250px] "></div>
-          </div>
-        </motion.div>
-        <motion.div variants={galleryVariant}>
-          <h2 className="my-3 text-lg font-semibold text-slate-700 dark:text-slate-50">Garda Azhari di Lakudo </h2>
-          <div className="w-full p-2 transition-colors duration-200 ease-in-out border border-slate-400 hover:bg-slate-400 dark:hover:bg-slate-400 dark:border-slate-400">
-            <div style={{ backgroundImage: "url('/img/galeri2.jpg')", backgroundSize: "cover", backgroundRepeat: "no-repeat" }} className="w-full min-h-[250px] "></div>
-          </div>
-        </motion.div>
-        <motion.div variants={galleryVariant}>
-          <h2 className="my-3 text-lg font-semibold text-slate-700 dark:text-slate-50">Kamomose Masyarakat Lakudo</h2>
-          <div className="w-full p-2 transition-colors duration-200 ease-in-out border border-slate-400 hover:bg-slate-400 dark:hover:bg-slate-400 dark:border-slate-400">
-            <div style={{ backgroundImage: "url('/img/galeri3.jpg')", backgroundSize: "cover", backgroundRepeat: "no-repeat" }} className="w-full min-h-[250px] "></div>
-          </div>
-        </motion.div>
+        {galleryPhoto.map((data) => (
+          <motion.div variants={galleryVariant} key={data.id}>
+            <h2 className="my-3 text-lg font-semibold text-slate-700 dark:text-slate-50">{data.title}</h2>
+            <div className="w-full p-2 transition-colors duration-200 ease-in-out border border-slate-400 hover:bg-slate-400 dark:hover:bg-slate-400 dark:border-slate-400">
+              <div style={{ backgroundImage: `url(${data.url})`, backgroundSize: "cover", backgroundRepeat: "no-repeat" }} className="w-full min-h-[250px] "></div>
+            </div>
+          </motion.div>
+        ))}
       </motion.div>
     </div>
   );
@@ -34,20 +25,22 @@ export const GalleryCardVideo = () => {
   return (
     <div>
       <motion.div variants={galleryContainerVariant} initial="hidden" whileInView="show" className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2">
+        {galleryVideo.map((data) => (
+          <motion.div variants={galleryVariant} key={data.id}>
+            <h2 className="my-3 text-lg font-semibold text-slate-700 dark:text-slate-50">{data.title}</h2>
+            <div className="w-full p-2 transition-colors duration-200 ease-in-out border border-slate-400 hover:bg-slate-400 dark:hover:bg-slate-400 dark:border-slate-400">
+              <iframe width="100%" height="315" src={data.url} title="YouTube video player" frameborder="0" allowfullscreen></iframe>
+            </div>
+          </motion.div>
+        ))}
         {/* <motion.div variants={galleryVariant}>
-          <h2 className="my-3 text-lg font-semibold text-slate-700 dark:text-slate-50">Lorem ipsum dolor sit.</h2>
-          <div className="w-full p-2 transition-colors duration-200 ease-in-out border border-slate-400 hover:bg-slate-400 dark:hover:bg-slate-400 dark:border-slate-400">
-            <iframe width="100%" height="315" src="https://www.youtube.com/embed/7I9kKCnd_Ug" title="YouTube video player" frameborder="0" allowfullscreen></iframe>
-          </div>
-        </motion.div> */}
-        <motion.div variants={galleryVariant}>
           <h2 className="my-3 text-lg font-semibold text-slate-700 dark:text-slate-50">Azhari yang Menginspirasi</h2>
           <div className="w-full p-2 transition-colors duration-200 ease-in-out border border-slate-400 hover:bg-slate-400 dark:hover:bg-slate-400 dark:border-slate-400">
-            <video src="/video/coba.mp4" className="w-full h-full" controls>
+            <video src="https://youtu.be/-Nec3XJLrKo" className="w-full h-full" controls>
               Your browser does not support the video tag.
             </video>
           </div>
-        </motion.div>
+        </motion.div> */}
       </motion.div>
     </div>
   );
---
+++ data/gallery.js
@@ -0,0 +1,35 @@
+export const galleryVideo = [
+  {
+    id: "1",
+    title: "Azhari yang Menginspirasi",
+    url: "https://www.youtube.com/embed/-Nec3XJLrKo",
+  },
+  {
+    id: "2",
+    title: "Dr. Azhari: Alasan USN B Tidak Dibangun Di Kolaka",
+    url: "https://www.youtube.com/embed/aCKuz8UU8pQ",
+  },
+  {
+    id: "3",
+    title: "Kepemimpinan & Kekuasaan",
+    url: "https://www.youtube.com/embed/kf5Q-qL7_q0",
+  },
+];
+
+export const galleryPhoto = [
+  {
+    id: "1",
+    title: "Silaturahmi Dengan Mahasiswa",
+    url: "/img/mhs.jpg",
+  },
+  {
+    id: "2",
+    title: "Garda Azhari di Lakudo",
+    url: "/img/galeri2.jpg",
+  },
+  {
+    id: "3",
+    title: "Kamomose Masyarakat Lakudo",
+    url: "/img/galeri3.jpg",
+  },
+];
