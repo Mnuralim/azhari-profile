766d5125ae6f2a7d58ecebe95d4cea0ce31bb266
mnuralim <shadownur345.com>
2023-06-15 00:42:16 +0800

    update

--- app/admin/riwayat/pendidikan/add/page.jsx
+++ app/admin/riwayat/pendidikan/add/page.jsx
@@ -104,7 +104,14 @@ const AddRiwayatPekerjaan = () => {
                 <label className="text-slate-500 md:text-lg dark:text-slate-200" htmlFor="gambar">
                   Gambar :
                 </label>
-                <input className="block w-full px-2 py-1 mt-2 text-sm rounded-md outline-none bg-slate-100 md:text-base dark:bg-slate-500" type="file" placeholder="gambar" id="gambar" onChange={(e) => setImage(e.target.files[0])} />
+                <input
+                  className="block w-full px-2 py-1 mt-2 text-sm rounded-md outline-none bg-slate-100 md:text-base dark:bg-slate-500"
+                  type="file"
+                  placeholder="gambar"
+                  id="gambar"
+                  onChange={(e) => setImage(e.target.files[0])}
+                  required
+                />
               </div>
 
               <div className="self-start mt-5">
