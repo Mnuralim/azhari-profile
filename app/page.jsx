86eb5b3b19b0227074aba2a74bfbf89bcec415ed
mnuralim <shadownur345.com>
2023-06-16 23:00:08 +0800

    update

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+"use client";
 import { HomePage } from "./home";
 import About from "./about";
 import Narasi from "./narasi";
@@ -6,8 +7,19 @@ import Gallery from "./gallery";
 import FullAbout from "./fullAbout";
 import Achievement from "./achievement";
 import IlmiahPage from "./ilmiahPage";
+import Statistic from "./statistic";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Home() {
+  const [data, setData] = useState("");
+  useEffect(() => {
+    const getData = async () => {
+      const res = await axios.put("https://colorful-calf-helmet.cyclic.app/update-stats");
+      setData(res.data);
+    };
+    getData();
+  }, []);
   return (
     <main className="overflow-x-hidden lg:px-36 ">
       <HomePage />
@@ -18,6 +30,7 @@ export default function Home() {
       <Narasi />
       <Gallery />
       <Aspiration />
+      <Statistic data={data} />
     </main>
   );
 }
---
+++ app/statistic.jsx
@@ -0,0 +1,39 @@
+"use client";
+import React from "react";
+
+const Statistic = ({ data }) => {
+  const updatedAt = data?.updatedAt;
+  const date = new Date(updatedAt);
+
+  // Daftar nama bulan dalam bahasa Indonesia
+  const bulan = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
+
+  // Mendapatkan jam dan menit dari objek Date
+  const jam = date.getHours();
+  const menit = date.getMinutes();
+
+  // Mendapatkan tanggal, bulan, dan tahun dari objek Date
+  const tanggal = date.getDate();
+  const namaBulan = bulan[date.getMonth()];
+  const tahun = date.getFullYear();
+
+  // Menggabungkan jam, menit, tanggal, bulan, dan tahun menjadi format yang diinginkan
+  const formattedDate = `jam ${jam}.${menit} WITA, ${tanggal} ${namaBulan} ${tahun}`;
+
+  console.log(formattedDate);
+
+  return (
+    <section id="award" className="relative flex items-center px-10 lg:px-24">
+      <div className="flex items-center justify-start w-full">
+        <div>
+          <div>
+            <p className="text-xl font-bold">Jumlah Kunjungan : {data.numbOfView}</p>
+            <p>Terakhir Dikunjungi : {formattedDate}</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Statistic;
